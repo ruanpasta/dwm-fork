1ebfcb47f0d4d810e389964cefa7acb71ea244f4
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2025-10-14 15:03:05 -0300

    fix: added comma

--- config.h
+++ config.h
@@ -98,7 +98,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0}}
+	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 	{ 0,                            XK_Print,  spawn,          {.v = flameshotcmd } },
 };
 
