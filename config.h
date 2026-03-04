a8a9c670a7d70a9ea1723a39646143d49cfbfa67
Ruan Pasta <ruan_augusto_pasta@hotmail.com>
2026-03-04 00:54:34 -0300

    feat: add default tab for teams

--- config.h
+++ config.h
@@ -35,7 +35,8 @@ static const Rule rules[] = {
 	{ "discord",        NULL,       NULL,       1 << 1,       0,            1 },
 	{ NULL, "steamwebhelper",       NULL,       1 << 2,       0,            1 },
 	{ "Slack",          NULL,       NULL,       1 << 3,       0,            1 },
-	{ "Spotify",        NULL,       NULL,       1 << 4,       0,            1 },
+	{ "teams-for-linux",NULL,       NULL,       1 << 4,       0,            1 },
+	{ "Spotify",        NULL,       NULL,       1 << 5,       0,            1 },
 	{ "Brave-browser",  NULL,       NULL,       0,            0,            0 },
 };
 
